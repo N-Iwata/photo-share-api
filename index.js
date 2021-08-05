c5f814e6e4ec87cfb48d3171243a104767d11ef3
Nobuhito Iwata <nobuhito323@gmail.com>
2021-08-06 00:25:34 +0900

    Add mongodb client

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+const { MongoClient } = require("mongodb");
 const { ApolloServer } = require(`apollo-server-express`);
 const expressPlayground = require("graphql-playground-middleware-express").default;
 const express = require("express");
 const { readFileSync } = require("fs");
+
+require("dotenv").config();
 
 const app = express();
 const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
 const resolvers = require("./resolvers");
 
 async function startApolloServer() {
+  const MONGO_DB = process.env.DB_HOST;
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
+  const db = client.db();
+  const context = { db };
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context,
   });
 
   await server.start();
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,7 +1,8 @@
 const { photos, users } = require("../dataset.js");
 
 module.exports = {
-  totalPhotos: () => 42,
-  allPhotos: () => photos,
-  allUsers: () => users,
+  totalPhotos: (parent, args, { db }) => db.collection("photos").estimatedDocumentCount,
+  allPhotos: (parent, args, { db }) => db.collection("photo").find().toArray(),
+  totalUsers: (parent, args, { db }) => db.collection("users").estimatedDocumentCount,
+  allUsers: (parent, args, { db }) => db.collection("users").find().toArray(),
 };
