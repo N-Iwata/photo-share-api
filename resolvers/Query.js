07880af225dac851f519ed5c94ff9a48770b8759
Nobuhito Iwata <nobuhito323@gmail.com>
2021-08-09 15:33:00 +0900

    Fix query function

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,8 +1,8 @@
 const { photos, users } = require("../dataset.js");
 
 module.exports = {
-  totalPhotos: (parent, args, { db }) => db.collection("photos").estimatedDocumentCount,
+  totalPhotos: (parent, args, { db }) => db.collection("photos").estimatedDocumentCount(),
   allPhotos: (parent, args, { db }) => db.collection("photo").find().toArray(),
-  totalUsers: (parent, args, { db }) => db.collection("users").estimatedDocumentCount,
+  totalUsers: (parent, args, { db }) => db.collection("users").estimatedDocumentCount(),
   allUsers: (parent, args, { db }) => db.collection("users").find().toArray(),
 };
